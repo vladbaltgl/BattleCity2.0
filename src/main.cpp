f71e95a1f01cb04b28e34166fc12479798f79281
Vladislav <vladbaltgl@gmail.com>
2022-10-27 07:39:37 +0400

    Add glm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include<glad/glad.h>
 #include <GLFW/glfw3.h>
+#include<glm/vec2.hpp>
+
 
 #include<iostream>
 
@@ -31,15 +33,13 @@ GLfloat texCoord[] = {
 
 
 
+glm::ivec2 g_windowSize(640, 480);
 
-
-int g_windowSizeX = 640;
-int g_windowSizeY = 480;
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow,int width,int height) {
-    g_windowSizeX = width;
-    g_windowSizeY = height;
-    glViewport(0, 0, g_windowSizeX, g_windowSizeY);
+    g_windowSize.x = width;
+    g_windowSize.y = height;
+    glViewport(0, 0, width, height);
 }
 void glfwKeyCallback(GLFWwindow* pWindow,int key,int scancode,int action,int mode) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
@@ -66,7 +66,7 @@ int main(int argc, char** argv)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* pWindow = glfwCreateWindow(g_windowSizeX, g_windowSizeY, "Battle City", nullptr, nullptr);
+    GLFWwindow* pWindow = glfwCreateWindow(g_windowSize.x, g_windowSize.y, "Battle City", nullptr, nullptr);
     if (!pWindow)
     {
         cout << "glfwCreateWindow failed\n";
