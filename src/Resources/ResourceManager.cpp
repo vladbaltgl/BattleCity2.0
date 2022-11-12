54f17b1a616e2e58c951029dc32ca40e0e978279
Vladislav <vladbaltgl@gmail.com>
2022-11-12 21:39:25 +0400

    Add class game

--- src/Resources/ResourceManager.cpp
+++ src/Resources/ResourceManager.cpp
@@ -13,11 +13,26 @@
 #include "stb_image.h"
 
 
-ResourceManager::ResourceManager(const std::string& executablePath) {
+
+  ResourceManager::ShaderProgramsMap ResourceManager::m_shaderPrograms;
+  ResourceManager::TexturesMap ResourceManager::m_textures;
+  ResourceManager::SpritesMap ResourceManager::m_sprites;
+  ResourceManager::AnimatedSpritesMap ResourceManager::m_animatedSprites;
+  std::string ResourceManager::m_path;
+
+  void  ResourceManager::unloadAllResources() {
+	  m_shaderPrograms.clear();
+	  m_textures.clear();
+	  m_sprites.clear();
+	  m_animatedSprites.clear();
+	  m_path.clear();
+  }
+
+void ResourceManager::setExecutablePath(const std::string& executablePath) {
 	std::size_t found = executablePath.find_last_of("/\\");
 	m_path = executablePath.substr(0,found);
 }
-std::string ResourceManager::getFileString(const std::string& relativeFilePath) const{
+std::string ResourceManager::getFileString(const std::string& relativeFilePath) {
 	std::ifstream f; 
 	f.open(m_path + "/" + relativeFilePath.c_str(),std::ios::in|std::ios::binary);
 	if (!f.is_open()) {
@@ -143,7 +158,7 @@ std::shared_ptr <Renderer::AnimatedSprite>ResourceManager::loadAnimatedSprite(co
 		std::cerr << "can't find the shader: " << shaderName << " for the sprite: " << spriteName << std::endl;
 
 	}
-	std::shared_ptr<Renderer::AnimatedSprite> newSprite = m_animatedSprites.emplace(textureName, std::make_shared<Renderer::AnimatedSprite>(pTexture,
+	std::shared_ptr<Renderer::AnimatedSprite> newSprite = m_animatedSprites.emplace(spriteName, std::make_shared<Renderer::AnimatedSprite>(pTexture,
 		subTextureName,
 		pShader,
 		glm::vec2(0.f, 0.f),
